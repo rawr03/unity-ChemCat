2caa0f27d5a6b277cc455312220296ad58951daf
kayfamorcan <kannefamorcan@gmail.com>
2023-10-12 14:52:04 +0800

    Standard Mode Scene
    The easy, medium, hard button are working.

--- ChemCat/Assets/LevelMenu.cs
+++ ChemCat/Assets/LevelMenu.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OpenLevel(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }

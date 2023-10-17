29058efc591f270f45a537fc29d28ee3c920471d
KristineAndreaTaborlupa <102679131+KrstAndng@users.noreply.github.com>
2023-10-17 18:25:44 +0800

    Added field for Equations

---
+++ ChemCat/Assets/Equations.cs
@@ -0,0 +1,12 @@
+
+[System.Serializable]
+public class Equations
+{
+    public int RT_coef1;
+    public string reactant1;
+    public int RT_coef2;
+    public string reactant2;
+    public int PD_coef1;
+    public string product1;
+
+}

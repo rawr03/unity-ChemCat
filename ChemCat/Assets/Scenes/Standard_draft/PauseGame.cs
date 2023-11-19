81914b10e737ecfb7a8b821fc694b39b15d0f632
KristineAndreaTaborlupa <102679131+KrstAndng@users.noreply.github.com>
2023-11-20 00:44:33 +0800

    Working Complete Pause Game Function

--- ChemCat/Assets/Scenes/Standard_draft/PauseGame.cs
+++ ChemCat/Assets/Scenes/Standard_draft/PauseGame.cs
@@ -5,26 +5,54 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    public GameObject settings;
+    public GameObject settings, reminder, instructions;
 
     public void Resume()
     {
-        settings.SetActive(false);
+        settings.gameObject.SetActive(false);
+        reminder.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(false);
+        //AudioManager.Instance.musicSource.UnPause();
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     public void Pause()
     {
-        settings.SetActive(true);
+        settings.gameObject.SetActive(true);
+        reminder.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(false);
+        //AudioManager.Instance.musicSource.Pause();
         Time.timeScale = 0f;  
+        GameIsPaused = true;
+    }
+
+    public void Exit()
+    {
+        settings.gameObject.SetActive(false);
+        reminder.gameObject.SetActive(true);
+        instructions.gameObject.SetActive(false);
+        //AudioManager.Instance.musicSource.Pause();
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Help()
+    {
+        settings.gameObject.SetActive(false);
+        reminder.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(true);
+        //AudioManager.Instance.musicSource.UnPause();
+        Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        reminder.gameObject.SetActive(false);
+        settings.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(false);
     }
 
     // Update is called once per frame

857a7d09b39a9502769a2f5343204c0589978723
kayfamorcan <144795941+kayfamorcan@users.noreply.github.com>
2023-11-20 04:01:14 +0800

    Transition1

---
+++ ChemCat/Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 5f;
+    
+
+
+    
+
+    public void LoadNextLevel(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}

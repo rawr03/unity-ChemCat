8200652a2463e114e0b71c032fbf5021a3d2d907
KristineAndreaTaborlupa <kristine.taborlupa@gmail.com>
2023-11-22 21:00:09 +0800

    During export

--- ChemCat/Assets/Dialogue/DialogueManager.cs
+++ ChemCat/Assets/Dialogue/DialogueManager.cs
@@ -8,7 +8,7 @@ using System.Xml.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 using UnityEngine.U2D;

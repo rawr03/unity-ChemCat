e6aa459fe7db124c1f55801eeacbf13591140888
KristineAndreaTaborlupa <102679131+KrstAndng@users.noreply.github.com>
2023-12-04 15:12:45 +0800

    Edits

--- ChemCat/Assets/Scenes/AAFINAL SCENES/FinalQuizControl.cs
+++ ChemCat/Assets/Scenes/AAFINAL SCENES/FinalQuizControl.cs
@@ -33,9 +33,8 @@ public class QuizControl : MonoBehaviour
     [SerializeField]
     private float timeBetweenEquations = .01f;
 
-    // anim
+    // animated time
     public TextMeshProUGUI addtnlTime;
-    //private Animator anim;
 
     // Timer
     public float time = 0f;
@@ -338,7 +337,7 @@ public class QuizControl : MonoBehaviour
             Highscore_HighscoreDisplay.text = PlayerPrefs.GetInt("NewHighScore", 0).ToString();
             //Debug.Log("NEW HIGH SCORE");
         }
-        else if (PlayerPrefs.GetInt("NewHighScore") > CurrScore)
+        else if (PlayerPrefs.GetInt("NewHighScore") >= CurrScore)
         {
             gameOver.gameObject.SetActive(true);
             GameOver_Score.text = CurrScore.ToString();
---
+++ ChemCat/Assets/Scenes/AAFINAL SCENES/QuizModeScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+
+public class QuizModeScript : MonoBehaviour
+{
+    public Equations[] problems;
+    private static List<Equations> unansweredProblems;
+
+    private Equations currentEquation;
+
+    // display Text of Equation Problem
+    [SerializeField]
+    private TextMeshProUGUI equationText1;
+
+    [SerializeField]
+    private TextMeshProUGUI equationText2;
+
+    [SerializeField]
+    private TextMeshProUGUI equationText3;
+
+    [SerializeField]
+    private TextMeshProUGUI equationText4;
+
+    [SerializeField]
+    private float timeBetweenEquations = .01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
